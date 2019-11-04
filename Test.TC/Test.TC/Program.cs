08fff5fad251b97f2702eec6f35d26e72cfd946f
sevap <sevap@DESKTOP-PS4FLU8>
2019-11-04 19:22:11 +0300

    add test 1

---
+++ Test.TC/Test.TC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test.TC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello TeamCity!");
+        }
+    }
+}
